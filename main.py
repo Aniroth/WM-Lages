279efd95af3b9917c9feb973ecea2661554a1ea0
Renan Otvin Klehm <renanotivin@hotmail.com>
2020-08-07 19:37:48 -0300

    UI updates

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication
-from GUI.MainGUI import Ui_MainWindow
+from GUI.MainWindow import Ui_MainWindow
 import sys
 
-class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
+class WMLagesApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
-        super(ExampleApp, self).__init__(parent)
+        super(WMLagesApp, self).__init__(parent)
         self.setupUi(self)
+        self.setupConnections(self)
+
+    def setupConnections(self, MainWindow):
+        MainWindow.PBT_Novo.clicked.connect(self.ClickTeste)
+    
+    def ClickTeste(self):
+        print("TESTE")
 
 def main():
     app = QApplication(sys.argv)
-    form = ExampleApp()
-    form.resize(800,600)
-    form.show()
+    app.setStyle('Fusion')
+    form = WMLagesApp()
+    form.showMaximized()
     app.exec_()
 
 if __name__ == '__main__':
