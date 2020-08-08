da7790a191dc9efed568c076fe4059640ef08c48
Renan Otvin Klehm <renanotivin@hotmail.com>
2020-08-08 13:26:39 -0300

    GUI Updates

---
+++ GUI/MainWindow/LogicMainWindow.py
@@ -0,0 +1,16 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
+from GUI.MainWindow.DrawMainWindow import Ui_MainWindow
+import sys
+
+class Logic_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(Logic_MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.setupConnections(self)
+
+    def setupConnections(self, MainWindow):
+        MainWindow.PBT_Novo.clicked.connect(self.ClickTeste)
+    
+    def ClickTeste(self):
+        print("TESTE")
--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication
-from GUI.MainWindow import Ui_MainWindow
+from GUI.MainWindow.LogicMainWindow import Logic_MainWindow
 import sys
-
-class WMLagesApp(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super(WMLagesApp, self).__init__(parent)
-        self.setupUi(self)
-        self.setupConnections(self)
-
-    def setupConnections(self, MainWindow):
-        MainWindow.PBT_Novo.clicked.connect(self.ClickTeste)
-    
-    def ClickTeste(self):
-        print("TESTE")
 
 def main():
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
-    form = WMLagesApp()
+    form = Logic_MainWindow()
     form.showMaximized()
     app.exec_()
 
